54fdd1c021f061d0e383da191c482d4d07f49769
kiwiidb <kwinten.debacker@hotmail.com>
2022-03-21 15:20:26 +0100

    primary login

--- api/callback.go
+++ api/callback.go
@@ -5,7 +5,7 @@ import (
 )
 
 func CallbackHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/xml")
+	w.Header().Set("Content-Type", "application/json")
 	w.Write([]byte(r.URL.Query().Get("user")))
 	w.WriteHeader(http.StatusOK)
 }
--- api/lnurlp.go
+++ api/lnurlp.go
@@ -1,11 +1,54 @@
 package handler
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
 )
 
+const (
+	HOST = "kwintendebacker.com"
+	MAX  = 1e6
+	MIN  = 1e4
+)
+
+type LNURLPayBody struct {
+	// callback
+	Callback string `json:"callback,omitempty"`
+	// comment allowed
+	CommentAllowed int32 `json:"commentAllowed,omitempty"`
+	// max sendable
+	MaxSendable int32 `json:"maxSendable,omitempty"`
+	// metadata
+	Metadata string `json:"metadata,omitempty"`
+	// min sendable
+	MinSendable int32 `json:"minSendable,omitempty"`
+	// tag
+	Tag string `json:"tag,omitempty"`
+}
+
 func LnUrlPHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	w.Write([]byte(r.URL.Query().Get("user")))
-	w.WriteHeader(http.StatusOK)
+	user := r.URL.Query().Get("user")
+	if user == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	response := &LNURLPayBody{
+		Callback:       fmt.Sprintf("https://%s/callback/%s", HOST, user),
+		CommentAllowed: 512,
+		MaxSendable:    MAX,
+		Metadata:       createLnurlMetadata(user),
+		MinSendable:    MIN,
+		Tag:            "payRequest",
+	}
+	err := json.NewEncoder(w).Encode(response)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+}
+
+func createLnurlMetadata(username string) (result string) {
+	return fmt.Sprintf("[[\"text/plain\", \"Payment to %s@%s\"]]", username, HOST)
+
 }
