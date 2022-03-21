1f27b6654ca6644a50be8176ad223d99ba6e79df
kiwiidb <kwinten.debacker@hotmail.com>
2022-03-21 15:34:09 +0100

    add callback

--- api/callback.go
+++ api/callback.go
@@ -1,11 +1,47 @@
 package handler
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
+	"strconv"
 )
+
+type CallBack struct {
+	// payment request
+	Pr string `json:"pr,omitempty"`
+	// routes, unused
+	Routes []interface{} `json:"routes"`
+}
 
 func CallbackHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	w.Write([]byte(r.URL.Query().Get("user")))
+	user := r.URL.Query().Get("user")
+	comment := r.URL.Query().Get("comment")
+	amount := r.URL.Query().Get("amount")
+	if user == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	intAmt, err := strconv.Atoi(amount)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	payload, err := createInvoicePayload(user, comment, intAmt)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	err = json.NewEncoder(w).Encode(payload)
+	if err != nil {
+		fmt.Println(err)
+	}
 	w.WriteHeader(http.StatusOK)
+}
+
+func createInvoicePayload(user, comments string, amount int) (result *CallBack, err error) {
+	return &CallBack{
+		Pr: "lnbctest123",
+	}, err
 }
